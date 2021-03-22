247e13cebd75e0e5fc823811caa8d6056f760363
AnnaPedych <apedych@gmail.com>
2021-03-22 23:49:12 +0200

    created plan for test cases

--- src/test/java/tests/ReqresTests.java
+++ src/test/java/tests/ReqresTests.java
@@ -1,5 +1,40 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class ReqresTests {
 
+    @BeforeAll
+    static void setup() {
+        RestAssured.filters(new AllureRestAssured());
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    @Test
+    void listAllResourcesTest() {
+
+    }
+
+    @Test
+    void verifyResourceParamsTest() {
+
+    }
+
+    @Test
+    void createUserTest() {
+
+    }
+
+    @Test
+    void updateUserTest() {
+
+    }
+
+    @Test
+    void deleteUserTest() {
+
+    }
 }
